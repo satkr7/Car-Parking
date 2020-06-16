7504b5bd11120c27f708999b981730edcb57cd21
satyam kumar <krsatyam1996@gmail.com>
2020-06-16 19:00:19 +0530

    lot booking update

--- booklot.php
+++ booklot.php
@@ -8,16 +8,23 @@ $uid = $_SESSION['log']['useruid'];
 $otp1 = mt_rand(100000,999999);
 $otp2 = mt_rand(100000,999999);
 
-$from = "contact.ecellvit@gmail.com";
+$from = "krsatyam1996@gmail.com";
 $email = $_SESSION['log']['email'];
 $recipient = $email;
 $subject = "Car Parking System OTP";
 $message = "Message via Car Parking System. This is your OTP1 (Enter while you park your car) ".$otp1." . This is your OTP2 (Enter while you leave the Parking Lot) ".$otp2." ." ;
 $headers = 'From:' . $from;
+
+$qry = mysqli_query($con,"INSERT INTO logtable (useruid, lotname, carno, otp1, otp2) VALUES ('$uid', '$lot', '$car', '$otp1', '$otp2') ");
+$qry1 = mysqli_query($con,"UPDATE lot SET status='Ongoing Booking' WHERE lotname='$lot' ");
+header("location:verify.php");
+
+/*
 if (mail($recipient, $subject, $message, $headers))
 {
     $qry = mysqli_query($con,"INSERT INTO logtable (useruid, lotname, carno, otp1, otp2) VALUES ('$uid', '$lot', '$car', '$otp1', '$otp2') ");
     $qry1 = mysqli_query($con,"UPDATE lot SET status='Ongoing Booking' WHERE lotname='$lot' ");
     header("location:verify.php");
 }
+*/
 ?>
--- config.php
+++ config.php
@@ -1,18 +1,22 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "");
+$con = mysqli_connect("localhost:3307", "root", "");
 
 if($con)
 {
-	$db = mysqli_select_db("carparkingsystem");
+	$db = mysqli_select_db($con, "carparkingsystem");
 	if(!$db)
 	{
 		echo "Could not connect to database".mysqli_error($con);
 	}
+	else
+	{
+		echo "Connected";
+	}
 }
 else
 {
-	echo "could not connect to server";
+	echo "Not Connected";
 }
 
 ?>
